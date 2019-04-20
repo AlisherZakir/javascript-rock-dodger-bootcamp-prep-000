f616d27d8b9e2e0add882e798d5350e53fa501c0
Alisher Zakir <a3zakir@gmail.com>
2019-04-20 19:17:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ function moveDodger(e) {
   const code = e.which
 
   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
-    e.preventDefault()
-    e.stopPropagation()
+    //e.preventDefault()
+    //e.stopPropagation()
   }
 
   if (code === LEFT_ARROW) {
